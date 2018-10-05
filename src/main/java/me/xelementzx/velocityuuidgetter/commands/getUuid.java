4f00d3e2c1b74a8370d862a3566c28ad760065c4
xElementzx <xelementzx1@gmail.com>
2018-10-05 21:44:31 +1000

    Initial Commit

---
+++ src/main/java/me/xelementzx/velocityuuidgetter/commands/getUuid.java
@@ -0,0 +1,26 @@
+package me.xelementzx.velocityuuidgetter.commands;
+
+import com.velocitypowered.api.command.Command;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.util.UuidUtils;
+import net.kyori.text.TextComponent;
+import net.kyori.text.format.TextColor;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class getUuid implements Command {
+
+    @Override
+    public void execute(@NonNull CommandSource source, String[] args) {
+        if (args.length != 1) {
+            source.sendMessage(TextComponent.of("Invalid Usage!").color(TextColor.RED));
+            source.sendMessage(TextComponent.of("Usage: /getuuid <player>").color(TextColor.RED));
+        }
+        if (args.length == 1) {
+            String playerName = args[0];
+            String playerOfflineUniqueID = UuidUtils.toUndashed(UuidUtils.generateOfflinePlayerUuid(playerName));
+            source.sendMessage(TextComponent.of("UUID of: " + playerName).color(TextColor.GREEN));
+            /*source.sendMessage(TextComponent.of("Online UUID: " + playerOnlineUniqueID).color(TextColor.GREEN));*/
+            source.sendMessage(TextComponent.of("Offline UUID: " + playerOfflineUniqueID).color(TextColor.RED));
+        }
+    }
+}
---
+++ src/main/java/me/xelementzx/velocityuuidgetter/lolnetuuidgetter.java
@@ -0,0 +1,23 @@
+package me.xelementzx.velocityuuidgetter;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.command.CommandManager;
+import me.xelementzx.velocityuuidgetter.commands.getUuid;
+import org.slf4j.Logger;
+
+@Plugin(id = "velocityuuidgetter", name = "VelocityUuidGetter", version = "1.0-SNAPSHOT",
+        description = "Meh, isn't it obvious", authors = {"xElementzx"})
+public class lolnetuuidgetter {
+    private final ProxyServer server;
+    private final Logger logger;
+    @Inject
+    public lolnetuuidgetter(CommandManager commandManager, ProxyServer server, Logger logger) {
+        commandManager.register(new getUuid(), "getUuid");
+        this.server = server;
+        this.logger = logger;
+
+        logger.info("Loaded!");
+    }
+}
