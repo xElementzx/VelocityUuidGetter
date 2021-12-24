9c41aeb854f506e9e07c630955b35e5d9a4a58ea
Gungadean <ryanjhuston@comcast.net>
2021-12-24 09:18:54 -0500

    Updated for Velocity API v3

--- src/main/java/me/xelementzx/velocityuuidgetter/commands/GetUuidCommand.java
+++ src/main/java/me/xelementzx/velocityuuidgetter/commands/GetUuidCommand.java
@@ -4,12 +4,11 @@ import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
 import com.velocitypowered.api.util.UuidUtils;
-import net.kyori.text.TextComponent;
-import net.kyori.text.format.TextColor;
-import org.checkerframework.checker.nullness.qual.NonNull;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -17,33 +16,37 @@ import java.net.URLConnection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class GetUuidCommand implements Command {
+public class GetUuidCommand implements SimpleCommand {
 
     @Override
-    public void execute(@NonNull CommandSource source, String[] args) {
-        if (source.hasPermission("velocityuuidgetter.command.getuuid")) {
-            if (args.length != 1) {
-                source.sendMessage(TextComponent.of("Invalid Usage!").color(TextColor.RED));
-                source.sendMessage(TextComponent.of("Usage: /getuuid <player>").color(TextColor.RED));
+    public void execute(Invocation invocation) {
+        CommandSource source = invocation.source();
+        String[] args = invocation.arguments();
+        if (args.length != 1) {
+            source.sendMessage(Component.text("Invalid Usage!", NamedTextColor.RED));
+            source.sendMessage(Component.text("Usage: /getuuid <player>", NamedTextColor.RED));
+        }
+        if (args.length == 1) {
+            String playerName = args[0];
+            String playerOfflineUniqueID = UuidUtils.toUndashed(UuidUtils.generateOfflinePlayerUuid(playerName));
+            String playerOnlineUniqueID = getOnlineUuid(playerName.toLowerCase()).flatMap(jsonObject -> parseJson(jsonObject.get("id"), String.class)).orElse(null);
+            source.sendMessage(Component.text("UUID of: " + playerName, NamedTextColor.GREEN));
+            if (playerOnlineUniqueID == null ) {
+                source.sendMessage(Component.text("Getting Online UUID Failed!", NamedTextColor.RED));
+            } else {
+                source.sendMessage(Component.text("Online UUID: " + playerOnlineUniqueID, NamedTextColor.GREEN));
             }
-            if (args.length == 1) {
-                String playerName = args[0];
-                String playerOfflineUniqueID = UuidUtils.toUndashed(UuidUtils.generateOfflinePlayerUuid(playerName));
-                String playerOnlineUniqueID = getOnlineUuid(playerName.toLowerCase()).flatMap(jsonObject -> parseJson(jsonObject.get("id"), String.class)).orElse(null);
-                source.sendMessage(TextComponent.of("UUID of: " + playerName).color(TextColor.GREEN));
-                if (playerOnlineUniqueID == null ) {
-                    source.sendMessage(TextComponent.of("Getting Online UUID Failed!").color(TextColor.RED));
-                } else {
-                    source.sendMessage(TextComponent.of("Online UUID: " + playerOnlineUniqueID).color(TextColor.GREEN));
-                }
-                source.sendMessage(TextComponent.of("Offline UUID: " + playerOfflineUniqueID).color(TextColor.GREEN));
-            }
-        } else {
-            source.sendMessage(TextComponent.of("You do not have permission to execute this command!").color(TextColor.RED));
+            source.sendMessage(Component.text("Offline UUID: " + playerOfflineUniqueID, NamedTextColor.GREEN));
         }
     }
 
-    private static Optional<JsonObject> getOnlineUuid(String username) {
+    @Override
+    public boolean hasPermission(final Invocation invocation) {
+        invocation.source().sendMessage(Component.text("You do not have permission to execute this command!", NamedTextColor.RED));
+        return invocation.source().hasPermission("velocityuuidgetter.command.getuuid");
+    }
+
+    private Optional<JsonObject> getOnlineUuid(String username) {
         try {
             URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + username);
             URLConnection urlConnection = url.openConnection();
@@ -56,15 +59,15 @@ public class GetUuidCommand implements Command {
         }
     }
 
-    private static <T> Optional<T> parseJson(String json, Class<T> type) {
+    private <T> Optional<T> parseJson(String json, Class<T> type) {
         try {
-            return parseJson(new JsonParser().parse(json), type);
+            return parseJson(JsonParser.parseString(json), type);
         } catch (RuntimeException ex) {
             return Optional.empty();
         }
     }
 
-    private static <T> Optional<T> parseJson(JsonElement jsonElement, Class<T> type) {
+    private <T> Optional<T> parseJson(JsonElement jsonElement, Class<T> type) {
         try {
             return Optional.of(new Gson().fromJson(jsonElement, type));
         } catch (RuntimeException ex) {
